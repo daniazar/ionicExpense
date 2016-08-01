15ff2cc83d61c52e54cf733b59da4795a118b898
Daniel Azar <daazar@usdev.deloitte.com>
2016-08-01 17:32:45 -0300

    added companies pages

---
+++ app/pages/companies-list/companies-list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the CompaniesListPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/companies-list/companies-list.html',
+})
+export class CompaniesListPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
---
+++ app/pages/company-details/company-details.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the CompanyDetailsPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/company-details/company-details.html',
+})
+export class CompanyDetailsPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
